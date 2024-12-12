21c30103628e64dd74a178f658b792dbca3fd674
Brandon Paul <brandon.p@accessitsoftware.com>
2024-12-13 09:52:48 +1300

    .

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,13 +1,6 @@
 import { sql } from 'drizzle-orm';
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
-export const user_table = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age'),
-	name: text('name'),
-	email: text('email'),
-});
-
 export const comments_table = sqliteTable('comments', {
 	id: integer('id').primaryKey(),
 	comment: text('comment'),
